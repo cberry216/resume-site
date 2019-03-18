dc3c3a06b5970a0cc3301c9ce1ddd793cdecea43
cberry216 <christopherberry216@gmail.com>
2019-03-18 09:57:48 -0400

    Created initial model for project.

--- resume_site/blog/models.py
+++ resume_site/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.CharField(max_length=50)
+    short_description = models.TextField()
+    long_description = models.TextField()
+    link_to = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('title')
+
+    def __str__(self):
+        return self.title
