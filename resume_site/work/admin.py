d3d5ed81ef40cf706aa84c03f4cb4f946c014ddb
cberry216 <christopherberry216@gmail.com>
2019-03-20 10:03:59 -0400

    Registered job model to admin site.

--- resume_site/work/admin.py
+++ resume_site/work/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Job
+
 # Register your models here.
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('company', 'title', 'slug', 'publish')
+    search_fields = ('company', 'title', 'description')
+    prepopulated_fields = {'slug': ('company', 'title')}
+    ordering = ('-publish',)
