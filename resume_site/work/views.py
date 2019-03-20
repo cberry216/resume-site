e4097460ce24eea9527da388df5f4551d03801cd
cberry216 <christopherberry216@gmail.com>
2019-03-20 09:32:42 -0400

    Created Job List view.

--- resume_site/work/views.py
+++ resume_site/work/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Job
 
 # Create your views here.
+
+
+class JobListView(ListView):
+    model = Job
+    template_name = 'work/work_list.html'
+    context_object_name = 'jobs'
