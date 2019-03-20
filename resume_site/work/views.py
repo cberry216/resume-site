70b290074ef9dfc33e519e922f0bde6d09396491
cberry216 <christopherberry216@gmail.com>
2019-03-20 09:33:18 -0400

    Added Job Detail view.

--- resume_site/work/views.py
+++ resume_site/work/views.py
@@ -9,3 +9,8 @@ class JobListView(ListView):
     model = Job
     template_name = 'work/work_list.html'
     context_object_name = 'jobs'
+
+
+class JobDetailView(DetailView):
+    model = Job
+    template_name = 'work/work_detail.html'
