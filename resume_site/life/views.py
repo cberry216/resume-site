9f7fdb5ffec4897f3529ed32db5938f96a67cd44
cberry216 <christopherberry216@gmail.com>
2019-03-21 14:45:34 -0400

    Added experience list and detail view.

--- resume_site/life/views.py
+++ resume_site/life/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Experience
 
 # Create your views here.
+
+
+class ExperienceListView(ListView):
+    model = Experience
+    template_name = 'life/experience_list.html'
+    context_object_name = 'experiences'
+
+
+class ExperienceDetailView(DetailView):
+    model = Experience
+    template_name = 'life/experience_detail.html'
