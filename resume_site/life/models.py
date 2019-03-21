6de1c7c1f326459cf809da825ab45f71451e37a0
cberry216 <christopherberry216@gmail.com>
2019-03-21 09:18:47 -0400

    Created experience model in life app.

--- resume_site/life/models.py
+++ resume_site/life/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Experience(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    image = models.ImageField(blank=True, null=True, upload_to='life/')
+    date_started = models.DateField()
+    date_ended = models.DateField(blank=True, null=True)
+    description = models.TextField()
+    long_description = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.name
