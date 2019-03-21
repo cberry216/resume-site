4526259e8fe3ee3137a478894716db8cca1e58d1
cberry216 <christopherberry216@gmail.com>
2019-03-21 09:26:56 -0400

    Made URL paths for education list and detail view

---
+++ resume_site/education/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (
+    EducationListView,
+    EducationDetailView,
+)
+
+app_name = 'education'
+
+urlpatterns = [
+    path('', EducationListView.as_view(), name='education_list'),
+    path('edu/<slug:slug>', EducationListView.as_view(), name='education_detail')
+]
