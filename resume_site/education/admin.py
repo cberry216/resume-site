eee5f11ec2a99f79a451a6836766e3fee837f5df
cberry216 <christopherberry216@gmail.com>
2019-03-21 09:17:21 -0400

    Registered education model with admin.

--- resume_site/education/admin.py
+++ resume_site/education/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Education
 # Register your models here.
+
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('college', 'slug', 'date_started', 'date_ended', 'publish')
+    search_fields = ('name', 'description', 'long_description')
+    prepopulated_fields = {'slug': ('college',)}
+    ordering = ('-publish',)
