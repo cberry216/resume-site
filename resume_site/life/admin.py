f0003b1d7381f147dd4712629d5f7d30990bd856
cberry216 <christopherberry216@gmail.com>
2019-03-21 09:19:43 -0400

    Registered experience model with admin.

--- resume_site/life/admin.py
+++ resume_site/life/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Experience
+
 # Register your models here.
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'date_started', 'date_ended', 'publish')
+    search_fields = ('name', 'description', 'long_description')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('-publish',)
