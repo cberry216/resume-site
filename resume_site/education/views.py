0b702cd426b26fe5ec1ea360ad68be241cc275ee
cberry216 <christopherberry216@gmail.com>
2019-03-21 09:26:25 -0400

    Created education list and detail view.

--- resume_site/education/views.py
+++ resume_site/education/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Education
 
 # Create your views here.
+
+
+class EducationListView(ListView):
+    model = Education
+    template_name = 'education/education_list.html'
+    context_object_name = 'colleges'
+
+
+class EducationDetailView(DetailView):
+    model = Education
+    template_name = 'education/education_detail.html'
