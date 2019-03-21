14b93b689ed76138ed8284877fcb0e5d7189ead6
cberry216 <christopherberry216@gmail.com>
2019-03-21 08:40:10 -0400

    Created education model.

--- resume_site/personal/models.py
+++ resume_site/personal/models.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 class Experience(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
-    image = models.ImageField(blank=True, null=True, upload_to="life/")
+    image = models.ImageField(blank=True, null=True, upload_to='life/')
     date_started = models.DateField()
     date_ended = models.DateField(blank=True, null=True)
     description = models.TextField()
@@ -19,3 +19,20 @@ class Experience(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Education(models.Model):
+    college = models.CharField(max_length=70)
+    slug = models.SlugField(max_length=70)
+    image = models.ImageField(blank=True, null=True, upload_to='college')
+    date_started = models.DateField()
+    date_ended = models.DateField(blank=True, null=True)
+    description = models.TextField()
+    long_description = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.college
