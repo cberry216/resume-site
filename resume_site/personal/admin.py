78f8b336e6615b24fe05c766ef086fd5728a635e
cberry216 <christopherberry216@gmail.com>
2019-03-21 08:47:26 -0400

    Registered education & experience models to admin.

--- resume_site/personal/admin.py
+++ resume_site/personal/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Experience, Education
+
 # Register your models here.
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'date_started', 'date_ended', 'publish')
+    search_fields = ('name', 'description', 'long_description')
+    prepopulated_fields = {'slug': ('name',)}
+    ordering = ('-publish')
+
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('college', 'slug', 'date_started', 'date_ended', 'publish')
+    search_fields = ('name', 'description', 'long_description')
+    prepopulated_fields = {'slug': ('college',)}
+    ordering = ('-publish')
