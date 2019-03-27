eebeadd5700a60bc996acef9d0178811ac4bfbbf
cberry216 <christopherberry216@gmail.com>
2019-03-27 16:53:02 -0400

    Swapping branches.

--- resume_site/resume_site/settings.py
+++ resume_site/resume_site/settings.py
@@ -24,7 +24,6 @@ MEDIA_URL = '/media/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
