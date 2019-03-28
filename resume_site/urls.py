cb16b39bfd5640e4ef4d73eeabc435e6e082d4b6
cberry216 <christopherberry216@gmail.com>
2019-03-28 12:22:30 -0400

    Removed more outdated media routes

--- resume_site/urls.py
+++ resume_site/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
-from django.views.static import serve
+# from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+# from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,7 +28,7 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += [
